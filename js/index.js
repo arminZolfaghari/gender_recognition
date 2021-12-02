61a33f081b8f85693a20ae8ad797b773bbf2072e
arminZolfaghari <arminzd1@gmail.com>
2021-12-03 00:20:51 +0330

    completed js file and added showMessage

--- js/index.js
+++ js/index.js
@@ -2,6 +2,16 @@ const URL = "https://api.genderize.io/";
 
 
 
+let showMessage = (message) => {
+    let alert = document.getElementById('alert');
+    alert.style.opacity = 1;
+    alert.innerHTML = message;
+    setTimeout(() => {
+        alert.style.opacity = 0;
+    }, 3000);
+}
+
+
 /**
  * send request to URL (https://api.genderize.io/) with param name
  * get json response
@@ -9,8 +19,14 @@ const URL = "https://api.genderize.io/";
  * @returns {Promise<Response>}
  */
 let sendRequest = async (name) => {
-    let res = await fetch(URL + `?name=${name}`);
-    return res.json()
+    try {
+        let res = await fetch(URL + `?name=${name}`);
+        return res.json()
+    }
+    catch (err){
+        showMessage(err)
+    }
+
 }
 
 
@@ -20,22 +36,71 @@ let sendRequest = async (name) => {
  * @param nameInfo
  */
 let setPrediction = (nameInfo) => {
-    let {gender, probability} = nameInfo;
-    console.log(gender)
-    console.log(probability)
-    document.getElementById("response-gender").innerHTML = `Gender: ${gender}`;
-    document.getElementById("response-prob").innerHTML = `Prob: ${probability}`;
+    let {name, gender, probability} = nameInfo;
+    if (gender) {
+        document.getElementById("response-gender").innerHTML = `Gender: ${gender}`;
+        document.getElementById("response-prob").innerHTML = `Prob: ${probability}`;
+        gender === 'male' ? document.getElementById("male-button").checked = true
+            : document.getElementById("female-button").checked = true
+    }
+    else
+        showMessage(`This name (${name}) doesn't exist!!!`)
 }
+
+
+
+/**
+ * show saved answer in the right bottom of page
+ * check local storage, if name exists => show gender
+ * else => show nothing
+ * @param name
+ */
+let showSavedAnswer = (name) => {
+    let resFromLocalStorage = getNameInfoFromLocalStorage(name);
+    if (resFromLocalStorage.gender)
+        document.getElementById('saved-answer-gender').innerHTML = resFromLocalStorage.gender;
+    else
+        document.getElementById('saved-answer-gender').innerHTML = "Nothing!";
+}
+
+
+/**
+ * @returns {name}
+ */
+let getNameFromInput = () => {
+    return document.getElementById("input-name").value;
+}
+
+
+/**
+ * check name with regex and length
+ * @param name
+ */
+let checkRuleForName = (name) => {
+    console.log(name)
+    if (!/^[a-zA-Z\s]*$/.test(name) === true)
+        throw Error(`input name(${name}) has bad character. change it.`)
+    if (name.length > 255)
+        throw Error(`input name(${name}) length is more than 255 character. change it.`)
+}
+
 
 /**
  * send request to URL and get response
  * then set prediction
  */
 let handleRequest = async () => {
-    let inputName = document.getElementById("input-name").value;
-    console.log("name ", inputName)
-    let nameInfo = await sendRequest(inputName)
-    setPrediction(nameInfo)
+    try{
+        let inputName = getNameFromInput()
+        checkRuleForName(inputName)
+        console.log("name ", inputName);
+        let nameInfo = await sendRequest(inputName);
+        setPrediction(nameInfo);
+        showSavedAnswer(inputName)
+    }
+    catch (err) {
+        showMessage(err)
+    }
 }
 
 
@@ -45,10 +110,61 @@ let handleRequest = async () => {
  * @returns {Promise<void>}
  */
 let onSubmit = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
     await handleRequest();
 }
 
+
+/**
+ * get name and then search in local storage
+ * if name exist in local storage => return {name, gender}
+ * else => return {name, null}
+ * @param name
+ * @returns {{gender: string, name: string}}
+ */
+let getNameInfoFromLocalStorage = (name) => {
+    return {name, gender: localStorage.getItem(name)}
+}
+
+
+/**
+ * remove name and gender from local storage
+ * @param name
+ */
+let deleteNameInfoFromLocalStorage = (name) => {
+    let resFromLocalStorage = getNameInfoFromLocalStorage(name);
+    if (resFromLocalStorage.gender)
+        localStorage.removeItem(name)
+}
+
+
+/**
+ * save name and gender to local storage
+ * @param nameInfo: {name: string, gender: string}
+ */
+let saveNameInfo = (nameInfo) => {
+    let {name, gender} = nameInfo
+    localStorage.setItem(name, gender)
+}
+
+
+let onSave = (event) => {
+    event.preventDefault();
+    let gender = document.querySelector('input[name="gender"]:checked').value;
+    let name = getNameFromInput();
+    saveNameInfo({name, gender});
+    let message = `Saved name: ${name}, gender: ${gender}`
+    showMessage(message)
+
+}
+
+
+let onClear = (event) => {
+    event.preventDefault();
+    let name = getNameFromInput();
+    deleteNameInfoFromLocalStorage(name);
+    showSavedAnswer(name);
+}
 
 
 
